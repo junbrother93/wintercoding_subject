66da1b1b48cc1a6a5bff04c6ff23cd72248c0d47
junbrother93 <junbrother93@gmail.com>
2018-11-01 15:46:20 +0900

    카페에서 한거 텍스트뷰, 버튼 이벤트

--- app/src/main/java/com/example/junbrother93/imagesearch/MainActivity.java
+++ app/src/main/java/com/example/junbrother93/imagesearch/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.junbrother93.imagesearch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    EditText eTxtSearch = (EditText)findViewById(R.id.txtSearch);
+    Button btnSearch = (Button)findViewById(R.id.btnSearch);
+    //ScrollView scrResult = (ScrollView)findViewById(R.id.scrResult);
+    //TextView txtResult = (TextView)findViewById(R.id.txtResult);
+
+    public void onClick(View view) {
+        String strSearch = "";
+        switch(view.getId())
+        {
+            case R.id.btnSearch:
+                strSearch.concat(eTxtSearch.getText().toString());
+                //txtResult.setText(strSearch);
+                break;
+        }
     }
 }
